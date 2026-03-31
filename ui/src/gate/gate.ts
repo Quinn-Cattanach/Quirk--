5ac02461120907c06e287f872593f3f2830c9f15
Quinn-Cattanach <qcattanach@gmail.com>
2026-03-31 11:01:38 -0400

    temp commit while I publish branches" "

--- ui/src/gate/gate.ts
+++ ui/src/gate/gate.ts
@@ -4,6 +4,19 @@ export class Gate {
     set latexString(newValue: string) {
         this.#latexString = this.#latexString;
         //
+
+    }
+
+    async #generateBitmapBuffer() {
+        const svg = await MathJax.t("\\frac{a}{b}");
+        const svgString = svg.outerHTML;
+
+        const img = new Image();
+        img.src = "data:image/svg+xml;base64," + btoa(svgString);
+
+        img.onload = () => {
+        ctx.drawImage(img, 0, 0);
+        };
     }
 
     svg: SVGElement; // store the MathJax
--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -6,8 +6,11 @@ import './index.css';
 import MathJax from 'mathjax';
 
 await MathJax.init({
-  loader: { load: ['input/tex', 'output/svg'] }
+  loader: { load: ['input/tex', 'output/svg'] } // specify input and output formats
 });
+
+const svg = await MathJax.tex2svgPromise('\\frac{1}{x^2-1}', { display: true });
+console.log(MathJax.startup.adaptor.serializeXML(svg));
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
